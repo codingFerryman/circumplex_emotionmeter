be6bf866afc811f804a347e8545648007d503b58
He Liu <heliu@heliu.name>
2021-11-21 16:05:29 +0100

    Update submodule in setup

--- setup_env.sh
+++ setup_env.sh
@@ -3,13 +3,17 @@
 # Specify the project path
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+# Pull submodules
+git submodule update --init --recursive
+
 # Upgrade the building tools
 pip install --upgrade pip setuptools wheel
 
 # Install the requirements
 pip install -r $SCRIPT_DIR/emotionmeter/requirements.txt
+pip install -r $SCRIPT_DIR/requirements.txt
 
 # Download NLTK stopwords and Spacy corpus
 python -c "import nltk; nltk.download('stopwords')"
 python -m spacy download "en_core_web_lg"
-python -m spacy download "en_core_web_sm"
+# python -m spacy download "en_core_web_sm"
